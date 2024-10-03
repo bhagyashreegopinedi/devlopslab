6fa3a493fc23ba6a39e86b43278c86e349241070
bhagyashreegopinedi <gopinedibhagya@gmail.com>
2024-10-03 10:11:13 +0530

    mul

---
+++ mul.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,mul;
+    printf("enter the value of A");
+    scanf("%d",&A);
+    printf("enter the value of B");
+    scanf("%d",&B);
+    mul=A*B;
+    printf("mul=%d",mul);
+    return 0;
+}
