16574430172e7e84a8c526dcc75b18c5059e69fb
bhagyashreegopinedi <gopinedibhagya@gmail.com>
2024-10-03 09:53:30 +0530

    first commit

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,sum;
+    printf("enter the value of A");
+    scanf("%d",&A);
+    printf("enter the  value of B");
+    scanf("%d",&B);
+    sum=A+B;
+    printf("sum=%d",sum);
+    return 0;
+    }
+    
