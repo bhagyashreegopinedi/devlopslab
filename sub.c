d7f2a877d37b573cadc2fa2c326cc0a735e20dc6
bhagyashreegopinedi <gopinedibhagya@gmail.com>
2024-10-03 10:16:19 +0530

    sub

---
+++ sub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,sub;
+    printf("enter the value of A");
+    scanf("%d",&A);
+    printf("enter the value of B");
+    scanf("%d",&B);
+    sub=A-B;
+    printf("sub=%d",sub);
+    return 0;
+}
